144496284c46f9ba8684c2efabd9e7c627dc8df3
mohammedabdelaleem <mohammedabdelaleem20@gmail.com>
2025-07-13 04:13:22 +0300

    simple updates

---
+++ WebStore.API/Errors/ShoppingCartErrors.cs
@@ -0,0 +1,22 @@
+﻿namespace WebStore.API.Errors;
+
+
+public record ShoppingCartErrors
+{
+
+	public static readonly Error NotFound
+		= new Error("ShoppingCart.NotFound", "ShoppingCart With Given Id Not Found", StatusCodes.Status404NotFound);
+
+
+	public static readonly Error ShoppingCartDuplicated
+		= new Error("ShoppingCart.Duplicated", "ShoppingCart With The Same Name Is Already Exists", StatusCodes.Status409Conflict);
+
+
+	public static readonly Error SavingError
+		= new Error("ShoppingCart.SavingError", "Error While Savnig ShoppingCart ", StatusCodes.Status500InternalServerError);
+
+	public static readonly Error InvalidInfo
+		= new Error("ShoppingCart.InvalidInfo", "Recheck The Info", StatusCodes.Status400BadRequest);
+
+
+}
--- WebStore.API/Services/ShoppingCartService.cs
+++ WebStore.API/Services/ShoppingCartService.cs
@@ -10,88 +10,95 @@ public class ShoppingCartService(
 
 	public async Task<Result> AddOrUpdateItemInCart(string userId, int menuItemId, int updatedQuantityBy, CancellationToken cancellationToken = default)
 	{
-		if (await _userManager.FindByIdAsync(userId) is not { })
-			return Result.Failure(UserErrors.UserNotFound);
-
-
-		var shoppingCartDb = await _context.ShoppingCarts.Include(s => s.CartItems).FirstOrDefaultAsync(s => s.UserId == userId, cancellationToken);
-
-		if (await _context.MenuItems.FirstOrDefaultAsync(m => m.Id == menuItemId, cancellationToken) is not { } menuItem)
-			return Result.Failure(MenuItemErrors.NotFound);
-
-
-		if (shoppingCartDb == null && updatedQuantityBy <= 0)
+		try
 		{
-			return Result.Failure(new Error("Invalid Quantity", "Quantity Can't Be Less Than Or Equal Zero", StatusCodes.Status400BadRequest));
-		}
+			if (await _userManager.FindByIdAsync(userId) is not { })
+				return Result.Failure(UserErrors.UserNotFound);
 
-		// create a new shopping cart and add new menu item	
-		if (shoppingCartDb == null && updatedQuantityBy > 0)
-		{
 
-			var newShoppingCart = new ShoppingCart { UserId = userId };
+			var shoppingCartDb = await _context.ShoppingCarts.Include(s => s.CartItems).FirstOrDefaultAsync(s => s.UserId == userId, cancellationToken);
 
-			await _context.AddAsync(newShoppingCart, cancellationToken);
-			await _context.SaveChangesAsync(cancellationToken);
+			if (await _context.MenuItems.FirstOrDefaultAsync(m => m.Id == menuItemId, cancellationToken) is not { } menuItem)
+				return Result.Failure(MenuItemErrors.NotFound);
 
-			var newCartItem = new CartItem
+
+			if (shoppingCartDb == null && updatedQuantityBy <= 0)
 			{
-				MenuItemId = menuItemId,
-				Quantity = updatedQuantityBy,
-				ShoppingCartId = newShoppingCart.Id,
-				MenuItem = null               // don't forget null menu item : to prevent create a new one
-			};
+				return Result.Failure(new Error("Invalid Quantity", "Quantity Can't Be Less Than Or Equal Zero", StatusCodes.Status400BadRequest));
+			}
 
-			await _context.AddAsync(newCartItem, cancellationToken);
-			await _context.SaveChangesAsync(cancellationToken);
-
-		}
-		else
-		{
-			// shopping cart already exists for this user
-			var cartItemInCart = shoppingCartDb.CartItems.FirstOrDefault(c => c.MenuItemId == menuItemId);
-			if (cartItemInCart == null)
+			// create a new shopping cart and add new menu item	
+			if (shoppingCartDb == null && updatedQuantityBy > 0)
 			{
 
-				// item doesn't exist in the current cart
+				var newShoppingCart = new ShoppingCart { UserId = userId };
+
+				await _context.AddAsync(newShoppingCart, cancellationToken);
+				await _context.SaveChangesAsync(cancellationToken);
+
 				var newCartItem = new CartItem
 				{
 					MenuItemId = menuItemId,
 					Quantity = updatedQuantityBy,
-					ShoppingCartId = shoppingCartDb.Id,
+					ShoppingCartId = newShoppingCart.Id,
 					MenuItem = null               // don't forget null menu item : to prevent create a new one
 				};
 
 				await _context.AddAsync(newCartItem, cancellationToken);
 				await _context.SaveChangesAsync(cancellationToken);
+
 			}
 			else
 			{
-				// item already exists in the cart and we need to update quantity 
-				var newQuantity = cartItemInCart.Quantity + updatedQuantityBy;
-
-				if (updatedQuantityBy == 0 || newQuantity <= 0)
+				// shopping cart already exists for this user
+				var cartItemInCart = shoppingCartDb.CartItems.FirstOrDefault(c => c.MenuItemId == menuItemId);
+				if (cartItemInCart == null)
 				{
-					// remove cart item from the cart , if it is the only item then also remove the cart  
-					_context.Remove(cartItemInCart);
 
-					if (shoppingCartDb.CartItems.Count() == 1)  // shopping cart saving at memory 
+					// item doesn't exist in the current cart
+					var newCartItem = new CartItem
 					{
-						_context.Remove(shoppingCartDb);
-					}
-					await _context.SaveChangesAsync(cancellationToken);
+						MenuItemId = menuItemId,
+						Quantity = updatedQuantityBy,
+						ShoppingCartId = shoppingCartDb.Id,
+						MenuItem = null               // don't forget null menu item : to prevent create a new one
+					};
 
+					await _context.AddAsync(newCartItem, cancellationToken);
+					await _context.SaveChangesAsync(cancellationToken);
 				}
 				else
 				{
-					cartItemInCart.Quantity = newQuantity;
-					await _context.SaveChangesAsync(cancellationToken);
+					// item already exists in the cart and we need to update quantity 
+					var newQuantity = cartItemInCart.Quantity + updatedQuantityBy;
+
+					if (updatedQuantityBy == 0 || newQuantity <= 0)
+					{
+						// remove cart item from the cart , if it is the only item then also remove the cart  
+						_context.Remove(cartItemInCart);
+
+						if (shoppingCartDb.CartItems.Count() == 1)  // shopping cart saving at memory 
+							_context.Remove(shoppingCartDb);
+
+
+						await _context.SaveChangesAsync(cancellationToken);
+					}
+					else
+					{
+						cartItemInCart.Quantity = newQuantity;
+						await _context.SaveChangesAsync(cancellationToken);
+					}
 				}
 			}
+
+			return Result.Success();
 		}
+		catch (Exception ex)
+		{
+			// logging 
 
-		return Result.Success();
-
+			return Result.Failure(ShoppingCartErrors.SavingError);
+		}
 	}
 
 }
